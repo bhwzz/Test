1f24af4df519973a34c90203e3a45e0db6f3c72a
wanglililian <1292298768@qq.com>
2019-07-12 16:48:08 +0800

    增加了“增删改查”接口

---
+++ BigProject02/src/ProjectServer/InformationOperate.java
@@ -0,0 +1,8 @@
+package ProjectServer;
+
+public interface InformationOperate {
+	public boolean Add(Object o);
+	public boolean Delete(String id);
+	public boolean Change(Object o);
+	public boolean Find(String id);
+}
